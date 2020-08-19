9c682a58818cdd3415f0e7010e5d77efd3356cdf
Kaustubh Wadagavi <kaustubhwadagavi@gmail.com>
2020-08-19 12:19:40 +0530

    Removed bugs from the script and created file to store data

--- VMAuthentication.sh
+++ VMAuthentication.sh
@@ -2,6 +2,10 @@
 
 
 SubString="Failed password"
+file="/home/kaustubh/Desktop/auth.log"
+iCnt=0
+userName=""
+ipAddress=""
 authenticateRoot()
 {
 	cd /var/log/
@@ -13,6 +17,8 @@ readTheFile()
 { 
 	cd /home/kaustubh/Desktop/
 	countFailedPassword=$(grep "Failed password" auth.log | wc -l)
+	touch output.txt
+        echo "UserName                                            IP Address                                       Failed Login Attempts">>output.txt
 }
 
 getTotalNumberofUnAuthenticatedAttempts()
@@ -26,43 +32,90 @@ getTotalNumberofUnAuthenticatedAttempts()
 	fi
 }
 
-getDataOfFailedAttempts()
+getDataOfFailedFirstAttempt()
 {
-	echo "Calculating Data"
-	file="/home/kaustubh/Desktop/auth.log"
-	while IFS=: read f1 f2 f3 f4  
-	do 
-	if [[ "$f4" == *"$SubString"* ]]
+	FIRSTLINE=`head -1 $file`
+      
+	if [[ "$FIRSTLINE" == *"$SubString"* ]]
 	then 
-	  failedData="$f4"
-	  splitData 
+	  echo "Calculating Data"
+	  failedData="$FIRSTLINE"
+	  arr=($failedData)
+	  userName="${arr[8]}"
+	  ipAddress="${arr[10]}"
 	fi
-	done <"$file"
-	 
+}
+
+getDataFromFile()
+{
+    while IFS=: read a1 a2 a3 a4
+    do 
+    	if [[ "$a4" == *"$SubString"* ]]
+	  	then
+	     failedDataOfFile="$a4"
+	     splitData
+	  fi
+    	done <"$file"
 }
 
 splitData()
 {
 	setDelimiter=' '
-	read -a arr <<< "$failedData"
-	echo "${arr[0]} ${arr[1]} ${arr[2]} ${arr[3]} ${arr[4]} ${arr[5]}"
-	userName="${arr[3]}"
-	ipAddress="${arr[5]}"
-	if [ "${arr[3]}" == $userName ]&&[ "${arr[5]}" == $ipAddress ]
+	read -a arr2 <<< "$failedDataOfFile"
+	if [[ "${arr2[3]}" == *"$userName"* ]]&&[[ "${arr2[5]}" == *"$ipAddress"* ]]
 	then
 	   a=`expr $a + 1`
-	   
-	else 
-	   b=$a
-	   a=0
-           echo "$b"
+	   echo $a
+	elif [[ "${arr2[3]}" != *"$userName"* ]]||[[ "${arr2[5]}" != *"$ipAddress"* ]]
+	then
+	   echo $a
+	   if [[ $a -ge 3 ]]
+	   then
+	       saveDataInFile
+	   fi
+	   a=1
+	   userName="${arr2[3]}"
+	   ipAddress="${arr2[5]}"
 	fi
-	   
+}
+
+saveDataInFile()
+{
+	echo "$userName                                            $ipAddress                                              $a">>output.txt
 }
 # authenticateRoot
 readTheFile
 getTotalNumberofUnAuthenticatedAttempts
-getDataOfFailedAttempts
+getDataOfFailedFirstAttempt
+getDataFromFile
+saveDataInFile
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
